bb8e73500e07b3b0401660e68f574de9e9f182ab
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-09-18 14:08:19 +0300

    1.1js

---
+++ 1.1 js/script.js
@@ -0,0 +1,20 @@
+let myString = "Hi";
+
+let myNumber = 50;
+
+
+const myObject = {
+    key1: '1',
+    key2: '2',
+    key3: '3'
+  };
+
+  const myBoolean = true;
+
+  const myNull = null;
+
+  let myUndefinedVariable;
+
+  const mySymbol = Symbol(' ');
+
+
