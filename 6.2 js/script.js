70c5ff4bf2a948137ad79494fb4dd8c4393092d9
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-09-19 12:00:28 +0300

    6.3js

---
+++ 6.2 js/script.js
@@ -0,0 +1,46 @@
+// const people = ["Greg", "Mary", "Devon", "James"];
+// people.splice(0, 1);
+// console.log(people);
+
+// // 
+
+// people.splice(3, 1);
+// console.log(people);
+
+// // 
+
+// people.unshift("Matt");
+// console.log(people);
+
+// // 
+
+// people.push("Eduard");
+// console.log(people);
+
+// // 
+
+const people = ["Greg", "Mary", "Devon", "James"];
+const copyWithoutMary = people.slice()
+console.log(copyWithoutMary);
+
+// 
+
+// const people = ["Greg", "Mary", "Devon", "James"];
+// const indexOfMary = people.indexOf("Mary");
+// console.log(indexOfMary);
+
+// // 
+
+// const people = ["Greg", "Mary", "Devon", "James"];
+// const indexOfFoo = people.indexOf("Foo");
+// console.log(indexOfFoo); // "Foo" is not found in the array
+
+// // 
+
+// let people = ["Greg", "Mary", "Devon", "James"];
+// const indexToRemove = people.indexOf("Devon");
+// if (indexToRemove !== -1) {
+//   people.splice(indexToRemove, 1);
+// people.splice(indexToRemove, 0, "Elizabeth", "Artie");
+// }
+// console.log(people);
---
+++ 6.3 js/script.js
@@ -0,0 +1,6 @@
+const stringsArray = ["Short", "Medium", "Long", "Very Long", "Super Long"];
+
+
+const lengthsArray = stringsArray.map(str => str.length);
+
+console.log(lengthsArray);
