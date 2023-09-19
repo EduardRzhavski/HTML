a062b784ec0db994f9b58800aaf84d4f02bbb033
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-09-19 13:07:00 +0300

    7.1js

---
+++ 7.1 js/7.1.js
@@ -0,0 +1,14 @@
+const array = [3,4,4,3,6,3];
+function getUnique(arr){
+    
+    let uniqueArr = [];
+    
+    // loop through array
+    for(let i of arr) {
+        if(uniqueArr.indexOf(i) === -1) {
+            uniqueArr.push(i);
+        }
+    }
+    console.log(uniqueArr);
+}
+getUnique(array);
