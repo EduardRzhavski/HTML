3e116a17991822a1f99dca749253524837b38335
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-10-01 20:37:19 +0300

    17.1

---
+++ 17.1 js/17.1.js
@@ -0,0 +1,37 @@
+// var b = 1;
+// function someFunction(number) {
+// function otherFunction(input) {
+// return b;
+// }
+// b = 5;
+// return otherFunction;
+// }
+// var firstResult = someFunction(9);
+// var result = firstResult(2);
+
+// // firstResult(2) returns the current value of b = 5. 
+
+// -------------------------------------------------
+
+// var a = 1;
+// function b2() {
+// a = 10;
+// return;
+// function a() { }
+// }
+// b2();
+// console.log(a);
+
+// // b2 is executed, it will log 1.
+
+// -------------------------------------------------
+
+// let i;
+// for (i = 0; i < 3; i++) {
+// const log = () => {
+// console.log(i);
+// }
+// setTimeout(log, 100);
+// }
+
+// //  loop reached final value of 2 -three times
