4c9a889a23a60902e64b4c64efce8d558802fa30
EduardRzhavski <142872678+EduardRzhavski@users.noreply.github.com>
2023-10-02 17:31:55 +0300

    18.2js

---
+++ 18.1 js/18.1.js
@@ -0,0 +1,44 @@
+// Select all elements not using the querySelector method
+
+// const bodyElement = document.body;
+
+// const elementsInsideBody = [];
+
+// for (let i = 0; i < bodyElement.childNodes.length; i++) {
+//   const childNode = bodyElement.childNodes[i];
+
+//   if (childNode.nodeType === 1) {
+//     elementsInsideBody.push(childNode);
+//   }
+// }
+
+// elementsInsideBody.forEach((element, index) => {
+//   console.log(`Element ${index + 1}:`, element);
+// });
+
+// ----------------------------------------------------------------
+
+// Select all elements using querySelector method
+
+// const elementsInsideBody = document.querySelectorAll('body *');
+
+// const elementsArray = Array.from(elementsInsideBody);
+
+// elementsArray.forEach((element, index) => {
+//   console.log(`Element ${index + 1}:`, element);
+// });
+
+// -------------------------------------------------------------------
+
+// Select all inputs and iterate over them with the forEach
+// method and print each one to the console. Using both
+// querySelectorAll and getElementsByTagName
+
+// const inputElementsByTagName = document.getElementsByTagName('input');
+
+// const inputArray = Array.from(inputElementsByTagName);
+
+// inputArray.forEach((element, index) => {
+//   console.log(`Input ${index + 1}:`, element);
+// });
+
---
+++ 18.2 js/18.2.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const h1Element = document.querySelector("h1");
+  
+    h1Element.style.color = "blue";
+  });
+
+//   -----------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const paragraphElement = document.querySelector("p.paragraph");
+  
+    paragraphElement.style.fontSize = "30px";
+  });
+
+//   ---------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const descriptionElement = document.querySelector("#description");
+  
+    descriptionElement.style.display = "inline-block";
+  });
+
+//   ---------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const bodyElement = document.body;
+  
+    bodyElement.style.backgroundColor = "green";
+  });
+
+//   -------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.title = "New Title";
+  });
+  
